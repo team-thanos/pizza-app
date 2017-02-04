3c99255b5641da3b3066bb59bc88e5ba09e3de00
Ich Dusche Nackt <ichduschenackt@gmail.com>
2017-02-04 20:24:42 +0100

    .

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,16 +2,27 @@ import { Component } from '@angular/core';
 
 import { AlertController, LoadingController, NavController, ToastController } from 'ionic-angular';
 
+import { ShoppingCartService } from '../../services/shopping-cart.service';
+import { SystemService } from '../../services/system.service';
+
 import { BrowseMenuPage }   from '../shop/browse-menu';
 import { BrowseStoresPage } from '../store-admin/browse-stores';
 
 @Component({
   selector   : 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers  : [ShoppingCartService, SystemService]
 })
 export class HomePage {
 
-    constructor(private alertCtrl: AlertController, private loadingCtrl: LoadingController, private navCtrl: NavController, private toastCtrl: ToastController) {
+    constructor(
+        private alertCtrl: AlertController,
+        private loadingCtrl: LoadingController,
+        private navCtrl: NavController,
+        private toastCtrl: ToastController,
+        private shoppingCartService: ShoppingCartService,
+        private systemService: SystemService
+    ) {
     }
 
     public browseMenuTapped() : void {
@@ -48,6 +59,20 @@ export class HomePage {
                             content: 'Setze System zurück...'
                         });
                         loadingIndicator.present();
+                        this.systemService.resetDb().subscribe(
+                            () => {
+                                // clear basket because all linked entities have become invalid
+                                this.shoppingCartService.clear();
+                                loadingIndicator.dismiss();
+                                let toast = this.toastCtrl.create({
+                                    message : 'Das System wurde erfolgreich zurückgesetzt.',
+                                    duration: 1000,
+                                    position: 'top'
+                                });
+                                toast.present();
+                            },
+                            error => console.log(error)
+                        );
                     }
                 }
             }]
